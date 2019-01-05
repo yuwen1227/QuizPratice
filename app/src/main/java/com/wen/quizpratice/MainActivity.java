0701b86aa95934b357ea249fd488471af9f4a9cb
yuwen1227 <coco4522@gmail.com>
2019-01-05 22:03:28 +0800

    1

--- app/src/main/java/com/wen/quizpratice/MainActivity.java
+++ app/src/main/java/com/wen/quizpratice/MainActivity.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     boolean logon = false;
-    ArrayList<>
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
